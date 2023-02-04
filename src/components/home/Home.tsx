202643ac995cfad5f46490fb31c5304de4fb3637
hanyeong0815 <yoehadu815@gmail.com>
2023-02-04 11:43:00 +0900

    fetch(login): 로그인 모달 적용

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -9,21 +9,21 @@ import MainButton from "@styles/button";
 import Modal from "@styles/modal";
 import NewBoard from "@components/board/NewBoard";
 import LoginForm from "@components/login/widgets/LoginForm";
+import SignUp from "@components/signup/SignUp";
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isLoginOpen, setIsLoginOpen] = useState<boolean>(false);
+  const [isSignupOpen, setIsSignupOpen] = useState<boolean>(false);
   const { LOGIN, SIGNUP, PW_CHK } = PATH;
-  const { isAuthenticated } = useAuth();
-  const [nickname, setNickname] = useState<string | null>(null);
-  const [isAuth, setIsAuth] = useState<boolean | null>(false);
+  const { username, token, isAuthenticated } = useAuth();
+  const [nickname, setNickname] = useState<string | null>(StorageManager.getItem("nickname") ?? null);
+  const [isAuth, setIsAuth] = useState<boolean | null>(StorageManager.getItem("username") != null ?? false);
 
 
   useLayoutEffect(() => {
-    if (!isAuthenticated) {
+    if (token == null || username == null) {
       return;
     }
-    const username = StorageManager.getItem("username");
-    const token = StorageManager.getItem("token");
     console.log("토큰은>>>>>", token, "이름은>>>>", username);
     const url = `http://localhost:8080/home?username=${username}`;
     axios
@@ -31,23 +31,27 @@ const Home = () => {
       .then((res) => res.data)
       .then((userInfo: userInfoReqRes) => {
         console.log(userInfo);
+        StorageManager.setItem("nickname", userInfo.nickname);
+        setIsAuth(true);
         setNickname(userInfo.nickname);
-        setIsAuth(isAuthenticated);
       })
       .catch(console.error);
   }, [isAuthenticated]);
 
   return (
     <div>
-      <div className="min-w-full flex flex-row justify-end p-4 gap-4">
-        {!isAuth && <Link to={LOGIN} className="underline text-blue-700">로그인</Link>}
+      <Modal isOpen={isLoginOpen} setIsOpen={setIsLoginOpen} className="fixed min-h-full">
+        <LoginForm setIsOpen={setIsLoginOpen}/>
+      </Modal>
+      <Modal isOpen={isSignupOpen} setIsOpen={setIsSignupOpen} className="fixed min-h-full">
+        <SignUp setIsOpen={setIsSignupOpen}/>
+      </Modal>
+      <div className="min-w-full flex flex-row justify-end p-4 gap-4 -z-10">
+        {!isAuth && <Link to="#" onClick={()=>{setIsLoginOpen(true)}} className="underline text-blue-700">로그인</Link>}
         {!isAuth && <Link to={SIGNUP} className="underline text-blue-700">회원가입</Link>}
         {isAuth && <Link to={PW_CHK}>마이페이지</Link>}
       </div>
       <main>
-        <Modal isOpen={isOpen} className="fixed min-h-full">
-          <LoginForm setIsOpen={setIsOpen}/>
-        </Modal>
         <div className="flex flex-row justify-between px-8">
           <h1 className="font-bold text-6xl">Main Page </h1>
           {isAuth && <p className="text-xl pt-4">어서오세요 {nickname}님</p>}
@@ -55,8 +59,6 @@ const Home = () => {
         <section>
           <article>
             <MainButton onClick={()=> {
-              setIsOpen(true);
-              console.log(isOpen);
             }
             }>노래 등록</MainButton>
           </article>
