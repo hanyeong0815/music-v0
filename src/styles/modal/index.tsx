b11429a17bd9e28004e3625ec697cfd6088b79b8
hanyeong0815 <yoehadu815@gmail.com>
2023-02-04 11:41:50 +0900

    style(modal): 모달 화면에 보이도록 스타일 수정

--- src/styles/modal/index.tsx
+++ src/styles/modal/index.tsx
@@ -1,18 +1,23 @@
-import {FunctionComponent as FC} from "react";
+import React, {FunctionComponent as FC} from "react";
 import {CommonDivProps} from "@models/common/props";
 import ModalPortalInterface from "@components/modal/ModalPortalInterface";
 
 export interface ModalProps extends CommonDivProps {
   isOpen: boolean;
+  setIsOpen:  React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const Modal:FC<ModalProps> = (props) => {
-  const { isOpen, children, className, ...restProp } = props;
+  const { isOpen, setIsOpen, children, className, ...restProp } = props;
 
   return (
     <ModalPortalInterface isOpen={isOpen}>
-      <div {...restProp} className={`border rounded-md shadow-md p-8 ${className}`}>
-        {children}
+      <div className="min-h-screen min-w-full flex justify-center fixed left-0 top-0">
+        <div onClick={()=>{setIsOpen(false)}} className="w-full h-full absolute bg-black opacity-50">
+        </div>
+        <div {...restProp} className={`items-center m-24 w-full min-h-full relative overflow-scroll border rounded-md shadow-md p-8 bg-gray-400 ${className}`}>
+          {children}
+        </div>
       </div>
     </ModalPortalInterface>
   );
